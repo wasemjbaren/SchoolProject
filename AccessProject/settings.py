c6fa80c534acf9dc4ba5c41ba44ee3b691581073
wasemjbaren <58600468+wasemjbaren@users.noreply.github.com>
2022-05-02 02:41:23 +0300

    Update settings.py

--- AccessProject/settings.py
+++ AccessProject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-dtujhl!$whmyly1f(sh-klg_uqt%6h9s1%ip0%q3(tt@&%v@vf
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','school-wasemapp.herokuapp.com']
 
 
 # Application definition
